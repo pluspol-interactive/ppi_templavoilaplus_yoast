daa1f28a7b88bd5ebfcc026ae8501b13994517b8
Alexander Opitz <opitz@pluspol.info>
2019-04-11 16:06:29 +0200

    [TASK] Mark compatible with TYPO3 v9LTS and yoast_seo 3.x, 4.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'TemplaVoila! Plus for Yoast SEO',
+    'title' => 'TemplaVoilà! Plus for Yoast SEO',
     'description' => 'Integration of Yoast SEO into TemplaVoilà! Plus page layout module.',
     'category' => 'misc',
     'version' => '0.3.1-dev',
@@ -9,13 +9,13 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 0,
     'author' => 'Alexander Opitz',
     'author_email' => 'opitz@pluspol-interactive.de',
-    'author_company' => 'PLUSPOL interactive',
+    'author_company' => 'PLUSPOL interactive GbR',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.2.99',
-            'typo3' => '7.6.0-8.7.99',
+            'php' => '5.5.0-7.3.99',
+            'typo3' => '7.6.0-9.5.99',
             'templavoilaplus' => '7.1.3-7.99.99',
-            'yoast_seo' => '1.1.0-2.99.99',
+            'yoast_seo' => '1.1.0-4.99.99',
         ],
     ],
     'autoload' => [
